c660736acb5cdee30e534279183fc29497b7afb8
krisaramis <kristina.kulitsch@gmail.com>
2020-06-07 01:15:20 +0300

    added class Calculator and used it's methods in Programm class

---
+++ HomeWorks/HW03.Task1/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW03.Task1
+{
+    class Calculator
+        {
+            public static int Sum(int x, int y)
+            {
+            return x + y;
+            }
+              public static int Subtraction(int x, int y)
+            {
+            return x -y;
+            }
+            public static int Multiplication(int x, int y)
+            {
+                return x*y;
+            }
+            public static int Devision(int x, int y)
+            {
+                return x/y;
+            }
+            public static int DevisionReminder(int x, int y)
+            {
+                return x % y;
+            }
+            public static double CircleArea(double radius)
+            {
+                return Math.Pow(radius, 2) *Math.PI;
+            }
+    }
+}
+
+
+
