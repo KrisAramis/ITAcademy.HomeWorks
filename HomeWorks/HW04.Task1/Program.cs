254a9aca12dd5cace75c86a80c141e9b6e6dc2aa
krisaramis <kristina.kulitsch@gmail.com>
2020-06-10 07:57:20 +0300

    added method that sums two inputted integers from user

---
+++ HomeWorks/HW04.Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW04.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Sum_result.Sum_ordinary();
+            Console.WriteLine("Result equals:{0}", result);
+        }
+    }
+}
+        
---
+++ HomeWorks/HW04.Task1/Sum_result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW04.Task1
+{
+    class Sum_result
+    {
+        public static int  Sum_ordinary()
+        {
+            Console.WriteLine("Input 1st number:");
+             int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input 2nd number:");
+             int num2 = Convert.ToInt32(Console.ReadLine());
+            return num1 + num2;
+        }
+    }
+     
+}
