2fd16c6f57cef07859e64c02a199eb71ccc33736
krisaramis <kristina.kulitsch@gmail.com>
2020-06-10 15:02:50 +0300

    added alphabet class that returns letters reversed

---
+++ HomeWorks/HW04.Task5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HW04.Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alphabet alpha = new Alphabet();
+            alpha.alphareverse();
+        }
+    }
+}
