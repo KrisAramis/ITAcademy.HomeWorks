d4c83cb350b98cca55c528554930dc956c82212d
krisaramis <kristina.kulitsch@gmail.com>
2020-06-17 18:58:18 +0300

    added ReverseArray for returning elements in reverse order

---
+++ HomeWorks/HW05.Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HW05.Task3
+{
+    class ReverseArray
+    {
+        static void Main(string[] args)
+        {
+            int[] reversearray = new int [] {1, 2, 3, 4, 5, 6, 7};
+            Array.Reverse(reversearray);
+            foreach(var i in reversearray)
+            {
+                Console.WriteLine($"reversearray[{i-1}]:{reversearray[i-1]}");
+            }
+        }
+    }
+}
