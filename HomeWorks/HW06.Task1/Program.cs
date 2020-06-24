7a9afdd1374530d43073253ea15f29a66b2205c2
krisaramis <kristina.kulitsch@gmail.com>
2020-06-24 16:30:48 +0300

    added first task with error. 's' isn't converted to 'a'

---
+++ HomeWorks/HW06.Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HW06.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Inputpoem = ReturnFormattedPoem.Askpoem();
+            ReturnFormattedPoem.Replaceletters(Inputpoem);
+        }
+    }
+}
---
+++ HomeWorks/HW06.Task1/ReturnFormattedPoem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW06.Task1
+{
+    public class ReturnFormattedPoem
+    {
+        public static string Askpoem()
+        {
+            Console.WriteLine("Input a poem by separating lines with semicolon:");
+            string InputPoem = Console.ReadLine();
+            return InputPoem;
+        }
+        public static void  Replaceletters(string InputPoem)
+        {
+                string modifiedInputPoem;
+                modifiedInputPoem = InputPoem.Replace('s', 'a').Replace('a','s');
+                Console.WriteLine(modifiedInputPoem);
+        }
+        public static void SeparatePoem(string Inputpoem)
+        {
+            string[] poem = Inputpoem.Split(';');
+            foreach (String s in poem)
+            Console.WriteLine(s);
+        }
+    }
+}
