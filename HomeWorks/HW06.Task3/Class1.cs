9f21253b43c7a2c34ed24b89b602a595bfc708ca
krisaramis <kristina.kulitsch@gmail.com>
2020-06-29 19:45:15 +0300

    couldn't solve the third task

---
+++ HomeWorks/HW06.Task3/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW06.Task3
+{
+    public class Rebuild
+    {
+        public static void Rebuildstring(string sringexample)
+        {
+            //        Дана символьная строка, содержащая хотя бы один символ '?'. Удалить все символы '!' или '.',
+            //расположенные до первого символа '?' и заменить все пробелы, расположенные за первым
+            //символом '?', на символ подчёркивания '_'.
+            //Исходная строка: “1a!2.3!!.. 4.!.?6 7! ..?”
+            //В данной задаче используйте StringBuilder.
+            string stringexample;
+
+            StringBuilder Examplestring = new StringBuilder();
+
+            Examplestring.Replace("!", "6");
+            Console.WriteLine(Examplestring);
+        }
+    }
+}
---
+++ HomeWorks/HW06.Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW06.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Examplestring = "1a!2.3!!..4.!.? 6 7!.. ?"";
+
+            Rebuild.Rebuildstring(examplestring)
+        }
+    }
+}
