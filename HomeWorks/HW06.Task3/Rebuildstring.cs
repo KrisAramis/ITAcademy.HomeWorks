4ff3ba67d0074c213a322e83f98769cef9904216
krisaramis <kristina.kulitsch@gmail.com>
2020-07-05 01:01:46 +0300

    bla

---
+++ HomeWorks/HW06.Task3/Rebuildstring.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace HW06.Task3
+{
+    public class ChangeString
+    {
+        public static void Rebuildstring(string example)
+        {
+            char changepointsymbol = '?';
+            int indexOfChar = example.IndexOf(changepointsymbol);
+            StringBuilder changedstring = new StringBuilder();
+            changedstring.Append(example);
+            changedstring.Replace(' ', '_', indexOfChar, example.Length-1- indexOfChar);
+            changedstring.Replace("!","", 0,indexOfChar).Replace(".", "", 0, indexOfChar);
+            Console.WriteLine(indexOfChar);
+            Console.WriteLine(changedstring);
+        }
+    }
+}
