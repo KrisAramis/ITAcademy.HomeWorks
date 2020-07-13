d118aa17050d137d10310ae0feeb07d37097f769
krisaramis <kristina.kulitsch@gmail.com>
2020-07-13 13:30:44 +0300

    added not fully correct task2. Coulnd't manage to input cnt -- frequency number-- in array

---
+++ HomeWorks/HW07.Task2/LettersFrequency.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW07.Task2
+{
+    sealed class LettersFrequency
+    {
+        public void CountLettersFrequency()
+        {
+            string str;
+            int i, cnt;
+            char [] finaloutput = new char[] { };
+            Console.WriteLine("Enter a sentence");
+            str = Console.ReadLine();
+            char ch;
+            
+                for (ch = (char)65; ch <= 90; ch++)
+                {
+                    cnt = 0;
+               
+                    for (i = 0; i < str.Length; i++)
+                    {
+
+                        if (ch == str[i] || (ch + 32) == str[i])
+                        {
+                            cnt++;
+                        }
+                    }
+                    if (cnt > 0)    
+                    {
+                        Console.WriteLine(ch + "=" + cnt);
+                    }
+                }
+            }
+    }
+}
---
+++ HomeWorks/HW07.Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HW07.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LettersFrequency aa = new LettersFrequency();
+            aa.CountLettersFrequency();
+        }
+    }
+}
